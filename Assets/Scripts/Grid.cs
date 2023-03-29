df018a7d143f3fd052563bef739ddf8f9bf988ba
Korpo-spec <casper22x@gmail.com>
2023-03-29 14:52:27 +0200

    Grid WIP

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -15,9 +15,11 @@ public class Grid<T>
     private float cellSize;
     private Vector3 origin;
     private Vector3 _normal;
+    private Quaternion _rotationQuat;
+    private Action<Vector2, T[,]> _onSetup;
 
     public event Action<T> OnGridValueChanged;
-    public Grid(int width, int height, float cellSize, Vector3 origin, Vector3 normal)
+    public Grid(int width, int height, float cellSize, Vector3 origin, Vector3 normal, Action<Vector2, T[,]> onSetup)
     {
         grid = new T[width, height];
         _textMeshes = new TextMesh[width, height];
@@ -27,7 +29,14 @@ public class Grid<T>
         this.origin = origin;
         this._normal = normal;
         _normal.Normalize();
-        _plane = new Plane(Vector3.forward, normal);
+        _onSetup = onSetup;
+        
+        _plane = new Plane(normal, origin);
+        float angle = Vector3.Angle(normal, Vector3.forward);
+        Vector3 rotationAxis = Vector3.Cross(normal, Vector3.forward);
+        _rotationQuat = Quaternion.AngleAxis(angle, rotationAxis);
+        Matrix4x4 mat = Matrix4x4.Rotate(_rotationQuat);
+        
         VisualizeGrid();
     }
 
@@ -40,16 +49,17 @@ public class Grid<T>
                 /*_textMeshes[j,i] = UtilsClass.CreateWorldText(grid[j, i].ToString(), null, new Vector3(cellSize * (j +0.5f), cellSize * (i+0.5f), 0), 7,
                     Color.white, TextAnchor.MiddleCenter);
                     */
-                Debug.DrawLine(origin+new Vector3(cellSize * j, cellSize * i, 0),origin+new Vector3(cellSize*(j+1), cellSize*(i), 0), Color.white, 100f);
-                Debug.DrawLine(origin+new Vector3(cellSize * j, cellSize * i, 0),origin+new Vector3(cellSize*(j), cellSize*(i+1), 0), Color.white, 100f);
+                _onSetup(new Vector2(j,i), grid);
+                Debug.DrawLine(_rotationQuat * (origin+new Vector3(cellSize * j, cellSize * i, 0)) ,_rotationQuat *(origin+new Vector3(cellSize*(j+1), cellSize*(i), 0)), Color.white, 100f);
+                Debug.DrawLine(_rotationQuat * (origin+new Vector3(cellSize * j, cellSize * i, 0)),_rotationQuat *(origin+new Vector3(cellSize*(j), cellSize*(i+1), 0)), Color.white, 100f);
                 if (j == grid.GetLength(0)-1)
                 {
-                    Debug.DrawLine(origin+new Vector3(cellSize * (j+1), cellSize * i, 0),origin+new Vector3(cellSize*(j+1), cellSize*(i+1), 0), Color.white, 100f);
+                    Debug.DrawLine(_rotationQuat *(origin+new Vector3(cellSize * (j+1), cellSize * i, 0)),_rotationQuat *(origin+new Vector3(cellSize*(j+1), cellSize*(i+1), 0)), Color.white, 100f);
                 }
                 
                 if (i == grid.GetLength(1)-1)
                 {
-                    Debug.DrawLine(origin+new Vector3(cellSize * j, cellSize * (i+1), 0),origin+new Vector3(cellSize*(j+1), cellSize*(i+1), 0), Color.white, 100f);
+                    Debug.DrawLine(_rotationQuat *(origin+new Vector3(cellSize * j, cellSize * (i+1), 0)),_rotationQuat *(origin+new Vector3(cellSize*(j+1), cellSize*(i+1), 0)), Color.white, 100f);
                 }
             }
         }
@@ -68,6 +78,11 @@ public class Grid<T>
         }
 
         return grid[x, y];
+    }
+
+    public T GetValue(Vector2 vector2)
+    {
+        return GetValue(Mathf.FloorToInt(vector2.x), Mathf.FloorToInt(vector2.y));
     }
 
     public void SetValue(int x, int y, T value)
@@ -94,7 +109,27 @@ public class Grid<T>
         if (_plane.Raycast(ray, out enter))
         {
             Debug.Log(ray.GetPoint(enter));
-            vec = ray.GetPoint(enter);
+            Vector3 rayHit = ray.GetPoint(enter);
+            vec.x = rayHit.x;
+            vec.y = rayHit.z;
+            
+            /*
+            Vector3 cameraX = Vector3.zero;
+            cameraX = camera.transform.right;
+            Vector3 normalX = Vector3.zero;
+            normalX = _normal;
+            
+            Vector3 cameraY = Vector3.zero;
+            cameraY = camera.transform.up;
+            Vector3 normalY = Vector3.zero;
+            normalY = _normal;
+            
+            Vector3 scaleVec = Vector3.zero;
+            vec.x *= Vector3.Angle(cameraX, normalX) / 90 + 1;
+            vec.y *= Vector3.Angle(cameraY, normalY) / 90 + 1;
+            Debug.Log(Vector3.Angle(cameraX, normalX) + " " + cameraX + " " + normalX);
+            Debug.Log(vec);
+            */
             return true;
         }
         vec = Vector2.zero;
--- Assets/Scripts/GridHolder.cs
+++ Assets/Scripts/GridHolder.cs
@@ -6,11 +6,18 @@ using UnityEngine;
 public class GridHolder : MonoBehaviour
 {
     // Start is called before the first frame 
-    private Grid<int> grid;
+    [SerializeField] private GameObject gridObj;
+    private Grid<GameObject> grid;
     void Start()
     {
-        grid = new Grid<int>(5,4,1,Vector3.zero, Vector3.up);
+        grid = new Grid<GameObject>(5,4,1,Vector3.zero, Vector3.up, OnSetup);
+        
         Debug.DrawLine(Vector3.zero, Vector3.right, Color.white, 100f);
+    }
+
+    private void OnSetup(Vector2 vec, GameObject[,] gameObjects)
+    {
+        gameObjects[(int)vec.x, (int)vec.y]= Instantiate(gridObj, new Vector3(vec.x + 0.5f, -0.5f, vec.y + 0.5f), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -18,10 +25,11 @@ public class GridHolder : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition+ new Vector3(0,0,5)));
+            //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition+ new Vector3(0,0,5)));
             grid.CameraRaycast(Camera.main, out Vector2 pos);
+            grid.GetValue(pos).GetComponent<Renderer>().material.color = Color.red;
             //grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition+ new Vector3(0,0,10)), 10);
-            
+
         }
     }
 }
